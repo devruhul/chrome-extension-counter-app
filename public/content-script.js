c13e0783f1c7ecad3c3cf9d4aa9216d97a4a474e
devruhul <ruhulamin262001@gmail.com>
2022-05-29 00:27:22 +0600

    added some changes

--- public/content-script.js
+++ public/content-script.js
@@ -1,5 +1,3 @@
-document.body.style.backgroundColor = 'orange';
-
 window.chrome.action.onClicked.addListener((tab) => {
     window.chrome.scripting.executeScript({
       target: { tabId: tab.id },
