4a15769659f62b37676e411e24fb359da58fa6eb
devruhul <ruhulamin262001@gmail.com>
2022-05-29 00:23:12 +0600

    added some changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 import Counter from './Counter/Counter';
 
 function App() {
-  const [url, setUrl] = useState('');
 
-  /**
-   * Get current URL
-   */
-  useEffect(() => {
-    const queryInfo = { active: true, lastFocusedWindow: true };
-
-    // eslint-disable-next-line no-undef
-    chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
-      const url = tabs[0].url;
-      setUrl(url);
-    });
-  }, []);
   return (
     <div className="App">
       <Counter />
-      <p>URL:</p>
-      <p>
-        {url}
-      </p>
     </div>
   );
 }
