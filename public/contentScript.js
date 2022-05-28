ee6d2b1e048f4aad7d14251ebbbd3fc1425a2a8f
devruhul <ruhulamin262001@gmail.com>
2022-05-28 22:05:49 +0600

    added some changes to manifest and index.html for testing

---
+++ public/contentScript.js
@@ -0,0 +1,6 @@
+var greeting = "hello, ";
+var button = document.getElementById("mybutton");
+button.person_name = "Bob";
+button.addEventListener("click", () =>
+  alert(greeting + button.person_name + ".")
+  , false);
