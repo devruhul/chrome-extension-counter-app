4d148f8cdb5f6521fc1330d5cdb5875280d6cb4b
devruhul <ruhulamin262001@gmail.com>
2022-05-28 21:18:01 +0600

    setting up manifest json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Home from './Pages/Home/Home';
+import Counter from './Pages/Home/Counter/Counter';
+// import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Counter />
     </div>
   );
 }
